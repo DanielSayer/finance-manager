6d4cf573e637c43e52290c1dd0d21604186b9155
Daniel Sayer <daniel.sayer01@gmail.com>
2023-06-29 18:44:54 +1000

    Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react"
+import Input from "./components/Input"
 import Button from "./components/Button"
 
 const App = (): React.ReactElement => {
-  return <Button onClick={() => console.log("Clicked")} />
+  const [userName, setUserName] = useState<string>("")
+  return (
+    <>
+      <Input
+        placeholder="Username"
+        value={userName}
+        type="password"
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <Button type="submit">Text</Button>
+    </>
+  )
 }
 
 export default App
